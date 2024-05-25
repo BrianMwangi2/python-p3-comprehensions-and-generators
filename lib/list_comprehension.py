fb7568b69c2083da270544056b629833edc562fe
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-26 00:06:39 +0300

    passes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+def return_evens(sequence):
+    return [x for x in sequence if x % 2 == 0]
 
-def return_evens(num_list):
-    pass
+# Example usage:
+# sequence = [1, 2, 3, 4, 5, 6]
+# print(return_evens(sequence))  # Output: [2, 4, 6]
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+
+    return [sentence + '!' for sentence in sentences]
+
+# Example usage:
+# sentences = ["Hello", "How are you", "This is great"]
+# print(make_exclamation(sentences))  # Output: ["Hello!", "How are you!", "This is great!"]
